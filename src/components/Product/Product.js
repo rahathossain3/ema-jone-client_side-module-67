1b3d5aad1fb657959b54ffc6b7e2fa16a7031ae1
rahathossain3 <rahathossain210@gmail.com>
2022-03-25 14:55:18 +0600

    single product add

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Product.css';
 
 const Product = (props) => {
-    console.log(props);
+    // console.log(props.product);
+    //destructuring object 
+    const { name, img, seller, price, ratings } = props.product;
+
     return (
-        <div>
-            <h2>This is Porduct</h2>
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
+
         </div>
     );
 };
